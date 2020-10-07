eabd83b8302605487a5fc4288078e647bc0d0bfe
Wizard Young <rcvbuf@gmail.com>
2020-10-08 04:45:54 +0800

    Add project files.

---
+++ Nebula/Store/BlockSearchUseCase/BlockSearchAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nebula.Store.BlockSearchUseCase
+{
+    public class BlockSearchAction
+    {
+        public string hash { get; }
+        public long height { get; }
+
+        public BlockSearchAction(string hashToSearch, long heightToSearch)
+        {
+            hash = hashToSearch;
+            height = heightToSearch;
+        }
+    }
+}
