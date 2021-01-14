9db7d099d1715f7b9dd452212b6b7503e065eadf
Wizard Young <rcvbuf@gmail.com>
2021-01-15 00:42:12 +0800

    show seeds addr in node view

--- Nebula/Pages/ShowBillBoard.razor.cs
+++ Nebula/Pages/ShowBillBoard.razor.cs
@@ -1,9 +1,11 @@
 ﻿using Fluxor;
+using Lyra.Data.API;
 using Microsoft.AspNetCore.Components;
 using Nebula.Store.NodeViewUseCase;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Nebula.Pages
@@ -16,10 +18,26 @@ namespace Nebula.Pages
 		[Inject]
 		private IDispatcher Dispatcher { get; set; }
 
+		private Dictionary<string, string> seedHosts;
+
 		protected override void OnInitialized()
 		{
 			base.OnInitialized();
 			Dispatcher.Dispatch(new NodeViewAction());
+
+			seedHosts = new Dictionary<string, string>();
+			_ = Task.Run(() => {
+				var seeds = LyraAggregatedClient.GetSeedNodes(Configuration["network"]);
+				foreach(var seed in seeds)
+                {
+					try
+					{
+						var ip = Dns.GetHostEntry(seed);
+						seedHosts.Add($"{ip.AddressList[0]}", seed);
+					}
+					catch { }
+                }					
+			});
 		}
 	}
 }
