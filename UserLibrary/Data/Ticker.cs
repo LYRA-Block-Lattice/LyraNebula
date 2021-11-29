e2862e9bbd0901752e0bbf80c4f8f7ced46c955a
Wizard Young <rcvbuf@gmail.com>
2021-11-30 03:33:51 +0800

    show ticker ok.

---
+++ UserLibrary/Data/Ticker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLibrary.Data
+{
+    public class Ticker
+    {
+        public string dbname { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
