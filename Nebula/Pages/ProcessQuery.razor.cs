e4555b55d19d0ede4595f60191aa3bcc38a3c7cb
Wizard Young <rcvbuf@gmail.com>
2022-01-03 00:39:47 +0800

    big split

---
+++ Nebula/Pages/ProcessQuery.razor.cs
@@ -0,0 +1,42 @@
+﻿using Fluxor;
+using Lyra.Core.API;
+using Lyra.Core.Blocks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+using Nebula.Store.StatsUseCase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nebula.Pages
+{
+    public partial class ProcessQuery
+    {
+		[Inject]
+		IConfiguration Configuration { get; set; }
+		string hash;
+		IEnumerable<TransactionBlock> transactions;
+		protected override void OnInitialized()
+		{
+			base.OnInitialized();
+		}
+
+		async void OnChange(string value)
+		{
+			if (value!.Length == 44)
+            {
+				var lc = LyraRestClient.Create(Configuration["network"], Environment.OSVersion.ToString(), "Nebula", "1.4");
+				var ret = await lc.GetBlocksByRelatedTxAsync(value);
+				if(ret.Successful())
+                {
+					transactions = ret.GetBlocks().Cast<TransactionBlock>();
+					StateHasChanged();
+					return;
+                }
+			}
+			transactions = null;
+
+		}
+	}
+}
